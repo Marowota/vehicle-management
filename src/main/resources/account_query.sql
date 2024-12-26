ffa5c1410c40b06aa0d2272c1424ba3087bc1a8c
Andrew Nguyen <21520144@gm.uit.edu.vn>
2024-12-27 00:50:00 +0700

    Add query files

---
+++ src/main/resources/account_query.sql
@@ -0,0 +1,6 @@
+INSERT INTO ACCOUNT (id, email, username, password, name, role, is_enabled)
+VALUES 
+(1, 'hello1@example.com', 'Anh123', '$2a$05$NtAgEM5Pbh30mFlU8CWwTOgMzbCXxhBUOy97v3UXynQU94MuNoNMm', 'Hoang Anh', 'TEACHER', true),
+(2, 'hello2@example.com', 'Manh123', '$2a$05$NtAgEM5Pbh30mFlU8CWwTOgMzbCXxhBUOy97v3UXynQU94MuNoNMm', 'Duc Manh', 'TEACHER', true),
+(3, 'hello3@example.com', 'Manh125', '$2a$05$NtAgEM5Pbh30mFlU8CWwTOgMzbCXxhBUOy97v3UXynQU94MuNoNMm', 'Manh Hoang', 'TEACHER', true),
+(4, 'hello4@example.com', 'Anh126', '$2a$05$NtAgEM5Pbh30mFlU8CWwTOgMzbCXxhBUOy97v3UXynQU94MuNoNMm', 'Nguyen Van Anh', 'TEACHER', true);
